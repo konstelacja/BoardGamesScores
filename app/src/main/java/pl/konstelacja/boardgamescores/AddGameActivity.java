60b5f5e7771d5319ec8a72737c74f21cf8f26d58
Malgorzata Krzysztof <malgorzata.m.krzysztof@gmail.com>
2019-05-11 14:46:52 +0200

    Added Room, used androidx, removed support library.

--- app/src/main/java/pl/konstelacja/boardgamescores/AddGameActivity.java
+++ app/src/main/java/pl/konstelacja/boardgamescores/AddGameActivity.java
@@ -1,15 +1,15 @@
 package pl.konstelacja.boardgamescores;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class AddGameActivity extends AppCompatActivity {
 
@@ -31,7 +31,7 @@ public class AddGameActivity extends AppCompatActivity {
                 if (nameText.length() > 0) {
                     Game game = new Game(nameText, gameDescription);
                     finish();
-                }else{
+                } else {
                     Context context = getApplicationContext();
                     CharSequence text = "Please input game of name...";
                     int duration = Toast.LENGTH_SHORT;
--- app/src/main/java/pl/konstelacja/boardgamescores/AddPlayerActivity.java
+++ app/src/main/java/pl/konstelacja/boardgamescores/AddPlayerActivity.java
@@ -2,13 +2,14 @@ package pl.konstelacja.boardgamescores;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class AddPlayerActivity extends AppCompatActivity {
 
